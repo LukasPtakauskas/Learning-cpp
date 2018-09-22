288ce7edbc3c4930d25c9209f5e68ad7d7b2e0b1
Lukas Ptakauskas <lukas.ptakauskas@gmail.com>
2018-09-23 01:25:22 +0300

    adding a new excercise

---
+++ 1Lab/1.2 area/rectArea.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	/* initializing variables*/
+	long long int a,b;
+	/* input variables*/
+	cin >> a >> b;
+
+
+/* output*/
+	cout  << a * b ;
+	
+
+	return 0;
+}
