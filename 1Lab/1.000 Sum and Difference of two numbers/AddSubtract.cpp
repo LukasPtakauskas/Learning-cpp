b117612f2f6daa49006eb9eb7e875a55175cc74e
Lukas Ptakauskas <lukas.ptakauskas@gmail.com>
2018-09-22 15:07:48 +0300

    1st labwork

---
+++ 1Lab/1.000 Sum and Difference of two numbers/AddSubtract.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+// sum and difference of two numbers
+int main(int argc, char** argv) {
+
+	int a,b;
+
+
+	cin >> a >> b;
+
+	cout << "Suma yra " << a + b << endl;
+	cout << "Skirtumas yra " << a - b << endl;
+	return 0;
+}
---
+++ 1Lab/1.1 perimeter/perimeter.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+//calculates perimeter
+int main(int argc, char** argv) {
+	/* initializing variables*/
+	int a,b;
+	/* input variables*/
+	cin >> a >> b;
+
+
+/* output*/
+	cout  << (a + b)*2;
+
+
+	return 0;
+}
