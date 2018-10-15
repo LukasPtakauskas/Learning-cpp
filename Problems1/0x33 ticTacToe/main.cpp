6431420a3c9731708eaae1fc500cf3991a62c109
Lukas Ptakauskas <lukas.ptakauskas@gmail.com>
2018-10-15 17:33:39 +0300

    some solved, one added

---
+++ Problems1/0x33 ticTacToe/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+/*
+Petriukas su Jonuku zaide zaidima tic-tac-toe ant 3x3 lenteles
+i tuscius langelius raso savo simbolius.
+zaidziama tol, kol kuris nors zaidejas upildo pilna eilute, stulpeli ar istizaine
+savo simboliais arba nebelieka laisvu langelius
+    Reik suzinoti laimetoja
+Input per tarpa pateikti devyni simboliai x:petriuko, o:joniuko,, -:tuscias
+Output isvesti laimetojo varda, lygiuju atveju isvesti "Lygiosios",
+o jeigu tokia partijos baigtis neimanoma, isvesti "Klaida". 
+*/
+
+int main (int argc, char *argv[]){
+
+  scanf();
+
+
+  printf();
+  return 0;
+}
