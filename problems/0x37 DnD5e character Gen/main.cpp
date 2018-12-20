3b8ac5f786fe6402e630905a7f53c91554b618e9
Lukas Ptakauskas <lukas.ptakauskas@gmail.com>
2018-12-21 00:31:16 +0200

    making a character creation thing step two. it is challange 0x37

--- problems/0x37 DnD5e character Gen/main.cpp
+++ problems/0x37 DnD5e character Gen/main.cpp
@@ -4,16 +4,9 @@
 #include <string>
 #include <cmath>
 #include <random>
+
 using namespace std;
 
-struct character{
-  string name;
-  int class;
-  int background;
-  int race;
-  int level,ac,mv,hpmax,hpcurrent,gold;
-  int stats[6],mods[6];
-}
 
 
 
@@ -25,64 +18,30 @@ int main() {
 
   double progress = .0, prevprogress = .0;
   int outputCounter = 0;
+  string input_file_name , output_file_name;
   string lineFromFile;
 
+  cout << "what is the input file?  ";
+  cin >> input_file_name;
+  cout << "what is the output file? ";
+  cin >> output_file_name;
+
   //this works better
-  ifstream myInput("data2.txt");
-  myInput.open("data2.txt");
+  ifstream myInput ;
+  myInput.open(input_file_name);
   cout << "\n\n\nopen input \n";
 
-  ofstream myOutput; // output file
-  myOutput.open("output.php");
+  ofstream myOutput ; // output file
+  myOutput.open(output_file_name);
   cout << "open Output\n";
   //the beggining
   myOutput << "<?php\n$stock=new array ();\n";
-
   while (!myInput.eof()) {
-    getline(myInput, lineFromFile[0]);
-    getline(myInput, lineFromFile[1]);
-    getline(myInput, lineFromFile[2]);
-    getline(myInput, lineFromFile[3]);
-    getline(myInput, lineFromFile[4]);
-    // sometimes it has an extra attribute: piercing slashing bludgeoning ac
-    if (lineFromFile[4] == "Piercing" || lineFromFile[4] == "Slashing" || lineFromFile[4] == "Bludgeoning" || (lineFromFile[4][0] == 'A' && lineFromFile[4][1] == 'C')) {
-      getline(myInput, lineFromFile[5]);
-      inputCounter += 6;
-      myOutput << "$stock[] = new array ('name'=>'" << lineFromFile[0] << "','type'=>'" << lineFromFile[1] << "','cost'=>'" << lineFromFile[2] << "','weight'=>'" << lineFromFile[3] << "','description'=>'" << lineFromFile[4] << "','notes'=>'" << lineFromFile[5] << "');" << endl;
-      cout << "+ one " << endl;
-    } else {
-      myOutput << "$stock[] = new array ('name'=>'" << lineFromFile[0] << "','type'=>'" << lineFromFile[1] << "','cost'=>'" << lineFromFile[2] << "','weight'=>'" << lineFromFile[3] << "','description'=>'','notes'=>'" << lineFromFile[4] << "');" << endl;
-      inputCounter += 5;
-    }
+    getline(myInput, lineFromFile);
 
-    //$stock[] = new array ('name'=>,'type'=>, 'price'=> ,'weigth'=>, 'category'=> );
-      //myOutput << "$stock[] = new array ('name'=>'" << lineFromFile[0] << "','type'=>,'" << lineFromFile[1] << "','cost'=>,'" << lineFromFile[2] << "','weight'=>,'" << lineFromFile[3] << "','notes'=>,'" << lineFromFile[4] << "');" << endl;
-    outputCounter++;
-
-    prevprogress = progress;
-    progress = double(inputCounter) / 1100.0;
-    if (prevprogress != progress)
-      cout << (floor(progress * 1000)) / 10 << " %\n";
   }
 
-  /*  double progress=.0, prevprogress=.0;
-    int inputCounter=0;
-    int outputCounter=0;
-    int linesize=0;
-    string lineFromFile, prev1, prev2;
-    string lineend;*/
-  cout << "vardump:\n";
-  cout << "progress" << " : " << progress << endl;
-  cout << "prevprogress" << " : " << prevprogress << endl;
-  cout << "inputCounter" << " : " << inputCounter << endl;
-  cout << "outputCounter" << " : " << outputCounter << endl;
-  cout << "linesize" << " : " << linesize << endl;
-  for (int i = 0; i < 5; i++) {
-    /* code */
-    cout << "lineFromFile" << " : " << lineFromFile[i] << endl;
-  }
 
-  cout << "lineend" << " : " << lineend << endl;
 
   myInput.close();
   cout << "close input\n";
